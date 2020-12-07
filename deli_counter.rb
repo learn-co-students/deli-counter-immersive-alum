731230a2a5c7c43aaf733cb839c5b14de089ff2d
pianoFF <fangfei1star@gmail.com>
2020-12-07 16:13:45 -0500

    progress

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+# katz_deli = []
+
+def line (queue)
+  if queue.length == 0
+    puts "The line is currently empty."
+  else
+    queue_list = ''
+    queue.each_index do |index|
+      queue_list += " #{index + 1}. #{queue[index]}"
+    end
+    puts "The line is currently:#{queue_list}"
+  end
+end 
